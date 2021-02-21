38b1d8f1efafc3daa9ff2290bb71aff05d9d5358
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2021-02-21 18:14:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var inputs_1="";
+var inputs_2="";
+
+function commitparagraph_1(){
+    for(i = 1;i <= 8; i++){
+        inputs_1.push(document.getElementById("para_1-input_" + i + ". ").value)
+    }
+    console.log(inputs_1);
+    document.getElementById("Final_Product_1").innerHTML=inputs_1;
+}
+function commitparagraph_2(){
+    for(i = 1;i <= 8; i++){
+        inputs_2.push(document.getElementById("para_2-input_" + i + ". ").value)
+    }
+    console.log(inputs_2);
+    document.getElementById("Final_Product_2").innerHTML=inputs;
+}
